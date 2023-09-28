c6751a208376926238589cc897b4f97b4c60f364
Pulkit Narula <43206958+pulkitnarula13@users.noreply.github.com>
2023-09-28 11:26:51 -0700

    Added basic styling to SignUp

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -12,24 +12,44 @@ function SignUp() {
         const password = passwordRef.current.value;
 
         try {
-            await auth.createUserWithEmailAndPassword(email, password);
+        await auth.createUserWithEmailAndPassword(email, password);
+        // User is signed up successfully
         } catch (error) {
-            console.error('Error signing up:', error.message);
+        console.error('Error signing up:', error.message);
         }
-    }
+    };
 
     return (
-        <div className="w-1/2 mx-auto">
-        <h2 className="text-2xl font-semibold mb-4">Sign Up</h2>
-        <form onSubmit={handleSignUp}>
+        <div className="min-h-screen flex items-center justify-center bg-blue-50">
+        <div className="bg-white w-full sm:w-96 p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold mb-4 text-center">Sign Up</h2>
+            <form onSubmit={handleSignUp}>
             <div className="mb-4">
-            <input type="email" placeholder="Email" ref={emailRef} className="w-full p-2 border rounded" required />
+                <input
+                type="email"
+                placeholder="Email"
+                ref={emailRef}
+                className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
+                required
+                />
             </div>
             <div className="mb-4">
-            <input type="password" placeholder="Password" ref={passwordRef} className="w-full p-2 border rounded" required />
+                <input
+                type="password"
+                placeholder="Password"
+                ref={passwordRef}
+                className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
+                required
+                />
             </div>
-            <button type="submit" className="bg-blue-500 text-white p-2 rounded">Sign Up</button>
-        </form>
+            <button
+                type="submit"
+                className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition duration-300 w-full"
+            >
+                Sign Up
+            </button>
+            </form>
+        </div>
         </div>
     );
 }
