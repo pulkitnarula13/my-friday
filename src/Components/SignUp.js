05630b77d9c2c0b08cf31f3edb80494cc3b3081c
Pulkit Narula <43206958+pulkitnarula13@users.noreply.github.com>
2023-10-04 13:34:23 -0700

    Updated Signup.js

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,67 +1,93 @@
 import React, { useRef, useState } from 'react';
+import './styles/SignUp.css'; // Import your CSS styles
 import { auth } from '../Firebase';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function SignUp() {
-    const emailRef = useRef();
-    const passwordRef = useRef();
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
 
-    const navigate = useNavigate(); // Initialize useNavigate
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-    const [userEmail, setUserEmail] = useState('');
+  const navigate = useNavigate(); // Initialize useNavigate
 
-    const handleSignUp = async (e) => {
-        e.preventDefault();
-        console.log(auth);
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        console.log(email + " " + password);
-        try {
-        await createUserWithEmailAndPassword(auth,email, password).then((userCredential) => {
-            console.log(userCredential);
-        });
-        // User is signed up successfully
-        setUserEmail(email); // Store the user's email
-        navigate('/welcome'); // Navigate to the welcome page
-        } catch (error) {
-        console.error('Error signing up:', error);
-        }
-    };
+  const [userEmail, setUserEmail] = useState('');
 
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-blue-50">
-        <div className="bg-white w-full sm:w-96 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-semibold mb-4 text-center">Sign Up</h2>
-            <form onSubmit={handleSignUp}>
-            <div className="mb-4">
-                <input
-                type="email"
-                placeholder="Email"
-                ref={emailRef}
-                className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
-                required
-                />
-            </div>
-            <div className="mb-4">
-                <input
-                type="password"
-                placeholder="Password"
-                ref={passwordRef}
-                className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
-                required
-                />
-            </div>
-            <button
-                type="submit"
-                className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition duration-300 w-full"
-            >
-                Sign Up
-            </button>
-            </form>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(auth);
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email + " " + password);
+    try {
+    await createUserWithEmailAndPassword(auth,email, password).then((userCredential) => {
+        console.log(userCredential);
+    });
+    // User is signed up successfully
+    setUserEmail(email); // Store the user's email
+    navigate('/welcome'); // Navigate to the welcome page
+    } catch (error) {
+    console.error('Error signing up:', error);
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit} className="signup-form">
+        <div className="form-group">
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
         </div>
+        <div className="form-group">
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
         </div>
-    );
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            ref={emailRef}
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            ref={passwordRef}
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
